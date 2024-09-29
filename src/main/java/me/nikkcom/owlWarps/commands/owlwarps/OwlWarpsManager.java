a615b0be890194a47d9fca1d7c5a809f3d2bd707
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2024-09-29 21:43:08 +0100

    Create OwlWarpsManager.java

---
+++ src/main/java/me/nikkcom/owlWarps/commands/owlwarps/OwlWarpsManager.java
@@ -0,0 +1,108 @@
+package me.nikkcom.owlWarps.commands.pwarp;
+
+import me.nikkcom.owlWarps.OwlWarps;
+import me.nikkcom.owlWarps.commands.SubCommand;
+import me.nikkcom.owlWarps.commands.owlwarps.VersionCommand;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * PwarpManager is handling pwarp commands.
+ * <p>
+ * This class manages subcommands associated
+ * with the pwarp command.
+ * </p>
+ */
+public class OwlWarpsManager implements CommandExecutor, TabCompleter {
+
+    private final OwlWarps owlWarps;
+    private final ArrayList<SubCommand> subCommands = new ArrayList<>();
+
+    /**
+     * Class constructor with the main class instance.
+     *
+     * @param owlWarps The instance of the main class plugin instance.
+     */
+    public OwlWarpsManager(OwlWarps owlWarps) {
+        this.owlWarps = owlWarps;
+
+        subCommands.add(new VersionCommand());
+    }
+
+    /**
+     * Handles the command execution for pwarp commands.
+     *
+     * @param sender The command sender (player or console).
+     * @param command The command that was executed.
+     * @param label The alias of the command that was used.
+     * @param args The arguments passed to the command.
+     * @return true if the command was successfully executed; false otherwise.
+     */
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (args.length == 0){
+
+                // Basic usage here
+            }
+            if (args.length > 0) {
+                for (int i = 0; i < subCommands.size(); i++){
+                    if (args[0].equalsIgnoreCase(subCommands.get(i).getName())) {
+                        getSubCommands().get(i).perform(player, args);
+                        return true;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        List<String> list = new ArrayList<>();
+
+        if (sender instanceof Player) {
+            if (command.getName().equalsIgnoreCase("owlwarps")){
+                for (SubCommand subCommand : getSubCommands()) {
+                    list.add(subCommand.getName());
+                }
+                Collections.sort(list);
+                // Player has not entered any characters
+                if (args.length == 0) {
+                    return list;
+
+                    // Player has started typing
+                } else if(args.length == 1) {
+                    for (String str : list) {
+                        if (!str.startsWith(args[0].toLowerCase())){
+                            list.remove(str);
+                        }
+                    }
+                    Collections.sort(list);
+                    return list;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Retrieves the list of subcommands associated with the pwarp command.
+     *
+     * @return An ArrayList of SubCommand objects.
+     */
+    public ArrayList<SubCommand> getSubCommands() {
+        return subCommands;
+    }
+
+
+}
