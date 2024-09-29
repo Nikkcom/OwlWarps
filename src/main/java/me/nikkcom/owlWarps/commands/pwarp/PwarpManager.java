aa35e3dcd106150d94dc293559b32d17c2e03b21
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2024-09-29 21:43:17 +0100

    Update PwarpManager.java

--- src/main/java/me/nikkcom/owlWarps/commands/pwarp/PwarpManager.java
+++ src/main/java/me/nikkcom/owlWarps/commands/pwarp/PwarpManager.java
@@ -2,12 +2,17 @@ package me.nikkcom.owlWarps.commands.pwarp;
 
 import me.nikkcom.owlWarps.OwlWarps;
 import me.nikkcom.owlWarps.commands.SubCommand;
+import me.nikkcom.owlWarps.commands.owlwarps.VersionCommand;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * PwarpManager is handling pwarp commands.
  * <p>
@@ -15,7 +20,7 @@ import java.util.ArrayList;
  * with the pwarp command.
  * </p>
  */
-public class PwarpManager implements CommandExecutor {
+public class PwarpManager implements CommandExecutor, TabCompleter {
 
     private final OwlWarps owlWarps;
     private final ArrayList<SubCommand> subCommands = new ArrayList<>();
@@ -27,6 +32,8 @@ public class PwarpManager implements CommandExecutor {
      */
     public PwarpManager(OwlWarps owlWarps) {
         this.owlWarps = owlWarps;
+
+        subCommands.add(new MenuCommand());
     }
 
     /**
@@ -44,7 +51,8 @@ public class PwarpManager implements CommandExecutor {
             Player player = (Player) sender;
             if (args.length == 0){
 
-                // Basic usage here
+            MenuCommand menuCommand = new MenuCommand();
+            menuCommand.perform(player, args);
             }
             if (args.length > 0) {
                 for (int i = 0; i < subCommands.size(); i++){
@@ -58,6 +66,20 @@ public class PwarpManager implements CommandExecutor {
         return true;
     }
 
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        List<String> list = new ArrayList<>();
+
+        if (sender instanceof Player) {
+            if (command.getName().equalsIgnoreCase("pwarp")){
+
+                return list;
+            }
+        }
+
+        return null;
+    }
+
     /**
      * Retrieves the list of subcommands associated with the pwarp command.
      *
@@ -66,4 +88,6 @@ public class PwarpManager implements CommandExecutor {
     public ArrayList<SubCommand> getSubCommands() {
         return subCommands;
     }
+
+
 }
