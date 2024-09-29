1b54ed1cd3f903d8db3dfe2d0abfb642acb0002c
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2024-09-29 21:43:37 +0100

    Update OwlWarps.java

--- src/main/java/me/nikkcom/owlWarps/OwlWarps.java
+++ src/main/java/me/nikkcom/owlWarps/OwlWarps.java
@@ -1,6 +1,8 @@
 package me.nikkcom.owlWarps;
 
 import me.nikkcom.owlWarps.GUI.events.MenuEvents;
+import me.nikkcom.owlWarps.commands.pwarp.OwlWarpsManager;
+import me.nikkcom.owlWarps.commands.pwarp.PwarpManager;
 import me.nikkcom.owlWarps.configuration.LocaleLoader;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -17,8 +19,14 @@ public final class OwlWarps extends JavaPlugin {
         // Registering event classes
         getServer().getPluginManager().registerEvents(new MenuEvents(), this);
 
+        getCommand("pwarp").setExecutor(new PwarpManager(this));
+        getCommand("pwarp").setTabCompleter(new PwarpManager(this));
+
+        getCommand("owlwarps").setExecutor(new OwlWarpsManager(this));
+        getCommand("owlwarps").setTabCompleter(new OwlWarpsManager(this));
+
         // Example of retrieving a message
-        String versionMessage = localeLoader.getMessage("version_message");
+        String versionMessage = localeLoader.getMessage("version-message");
         getLogger().info(versionMessage != null ? versionMessage : "Message not found.");
     }
 
