2c3f9a8c5e761d59d62c1569f9fdca7b343f0542
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2024-09-29 21:43:32 +0100

    Create PaginatedMenu.java

---
+++ src/main/java/me/nikkcom/owlWarps/GUI/menus/PaginatedMenu.java
@@ -0,0 +1,12 @@
+package me.nikkcom.owlWarps.GUI.menus;
+
+import me.nikkcom.owlWarps.GUI.Menu;
+
+public class PaginatedMenu extends Menu {
+    public PaginatedMenu(int size, String name, String viewerID) {
+        super(size, name, viewerID);
+    }
+
+
+    // Slots in the config for the slots which are paginated.
+}
