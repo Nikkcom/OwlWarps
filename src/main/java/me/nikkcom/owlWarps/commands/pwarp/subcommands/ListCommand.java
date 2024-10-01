6fb93062668855de91875bef541fa402d44c425a
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2024-10-01 18:37:21 +0100

    Create ListCommand.java

---
+++ src/main/java/me/nikkcom/owlWarps/commands/pwarp/subcommands/ListCommand.java
@@ -0,0 +1,52 @@
+package me.nikkcom.owlWarps.commands.pwarp.subcommands;
+
+import me.nikkcom.owlWarps.OwlWarps;
+import me.nikkcom.owlWarps.commands.SubCommand;
+import me.nikkcom.owlWarps.playerwarps.PlayerWarp;
+import me.nikkcom.owlWarps.playerwarps.PlayerWarpManager;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ListCommand extends SubCommand {
+
+    private final OwlWarps owlWarps;
+    public ListCommand(OwlWarps owlWarps) {
+        this.owlWarps = owlWarps;
+    }
+
+    @Override
+    public String getName() {
+        return "list";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Lists all playerwarps.";
+    }
+
+    @Override
+    public String getSyntax() {
+        return "/pwarp list";
+    }
+
+    @Override
+    public void perform(Player player, String[] args) {
+        // Check permission
+        PlayerWarpManager manager = owlWarps.getPlayerWarpManager();
+        player.sendMessage("Playerwarps:");
+        for (PlayerWarp warp : manager.getWarps()) {
+            player.sendMessage(warp.getRawName());
+        }
+    }
+
+
+
+    @Override
+    public List<String> onTabComplete(Player player, String[] args) {
+        return Collections.emptyList();
+    }
+}
