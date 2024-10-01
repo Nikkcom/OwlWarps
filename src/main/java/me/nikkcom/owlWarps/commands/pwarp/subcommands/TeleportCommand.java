90362c5ad812dad65bdcb7dd934202b6e354fad3
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2024-10-01 18:37:24 +0100

    Create TeleportCommand.java

---
+++ src/main/java/me/nikkcom/owlWarps/commands/pwarp/subcommands/TeleportCommand.java
@@ -0,0 +1,78 @@
+package me.nikkcom.owlWarps.commands.pwarp.subcommands;
+
+import me.nikkcom.owlWarps.OwlWarps;
+import me.nikkcom.owlWarps.commands.SubCommand;
+import me.nikkcom.owlWarps.playerwarps.PlayerWarpManager;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+public class TeleportCommand extends SubCommand {
+
+    private final OwlWarps owlWarps;
+
+    public TeleportCommand(OwlWarps owlWarps) {
+        this.owlWarps = owlWarps;
+    }
+
+    @Override
+    public String getName() {
+        return "teleport";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Teleports the player to the specified warp";
+    }
+
+    @Override
+    public String getSyntax() {
+        return "/pwarp teleport <warp>";
+    }
+
+    @Override
+    public void perform(Player player, String[] args) {
+        PlayerWarpManager manager = owlWarps.getPlayerWarpManager();
+
+        if (args.length != 2) {
+            player.sendMessage(getSyntax());
+            return;
+        }
+        String warpName = args[1];
+
+        if (!manager.isWarp(warpName)) {
+            player.sendMessage("NIKO - warp is not a valid warp");
+            return;
+        }
+
+        manager.teleport(player, warpName);
+    }
+
+
+
+    public List<String> onTabComplete(Player player, String[] args) {
+        List<String> completions = new ArrayList<>();
+        Set<String> warps = owlWarps.getPlayerWarpManager().getPlayerWarpsByName().keySet();
+        completions.addAll(warps);
+        if (args.length == 1) {
+            return completions;
+        } else if (args.length == 2) {
+
+            for (String warp : completions) {
+                if (!warp.startsWith(args[1].toLowerCase())){
+                    completions.remove(warp);
+                }
+            }
+            Collections.sort(completions);
+            return completions;
+        }
+
+        return Collections.emptyList();
+    }
+}
