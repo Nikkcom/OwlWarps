cb4ff4938acfebf50d78ec6ec09c9f3a34e31c58
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2024-10-01 18:37:43 +0100

    Update OwlWarps.java

--- src/main/java/me/nikkcom/owlWarps/OwlWarps.java
+++ src/main/java/me/nikkcom/owlWarps/OwlWarps.java
@@ -1,14 +1,18 @@
 package me.nikkcom.owlWarps;
 
 import me.nikkcom.owlWarps.GUI.events.MenuEvents;
+import me.nikkcom.owlWarps.commands.pw.PwManager;
 import me.nikkcom.owlWarps.commands.pwarp.OwlWarpsManager;
 import me.nikkcom.owlWarps.commands.pwarp.PwarpManager;
 import me.nikkcom.owlWarps.configuration.LocaleLoader;
+import me.nikkcom.owlWarps.playerwarps.PlayerWarpManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class OwlWarps extends JavaPlugin {
 
     private LocaleLoader localeLoader;
+
+    private PlayerWarpManager playerWarpManager;
 
     @Override
     public void onEnable() {
@@ -16,6 +20,10 @@ public final class OwlWarps extends JavaPlugin {
         // Initializes the plugin messages loader.
         localeLoader = new LocaleLoader(this);
         localeLoader.loadMessages("locale/en_US.json");
+
+        playerWarpManager = new PlayerWarpManager(this);
+
+
         // Registering event classes
         getServer().getPluginManager().registerEvents(new MenuEvents(), this);
 
@@ -25,6 +33,8 @@ public final class OwlWarps extends JavaPlugin {
         getCommand("owlwarps").setExecutor(new OwlWarpsManager(this));
         getCommand("owlwarps").setTabCompleter(new OwlWarpsManager(this));
 
+        getCommand("pw").setExecutor(new PwManager(this));
+        getCommand("pw").setTabCompleter(new PwManager(this));
         // Example of retrieving a message
         String versionMessage = localeLoader.getMessage("version-message");
         getLogger().info(versionMessage != null ? versionMessage : "Message not found.");
@@ -33,5 +43,9 @@ public final class OwlWarps extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public PlayerWarpManager getPlayerWarpManager() {
+        return playerWarpManager;
     }
 }
