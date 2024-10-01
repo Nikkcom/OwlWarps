f7c94020e0f82ec84248dee0d2caadf1c023acd3
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2024-10-01 18:37:05 +0100

    Update VersionCommand.java

--- src/main/java/me/nikkcom/owlWarps/commands/owlwarps/VersionCommand.java
+++ src/main/java/me/nikkcom/owlWarps/commands/owlwarps/VersionCommand.java
@@ -2,7 +2,11 @@ package me.nikkcom.owlWarps.commands.owlwarps;
 
 import me.nikkcom.owlWarps.commands.SubCommand;
 import me.nikkcom.owlWarps.configuration.Messages;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.List;
 
 public class VersionCommand extends SubCommand {
     @Override
@@ -23,5 +27,11 @@ public class VersionCommand extends SubCommand {
     @Override
     public void perform(Player player, String[] args) {
         player.sendMessage(Messages.getMessage("version-message"));
+    }
+
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, String[] args) {
+        return List.of();
     }
 }
