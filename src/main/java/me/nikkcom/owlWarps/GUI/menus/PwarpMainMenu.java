3bfe0c57564982dca5dd3710b33fccd1cd4a7df6
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2024-10-01 18:58:29 +0100

    Update PwarpMainMenu.java

--- src/main/java/me/nikkcom/owlWarps/GUI/menus/PwarpMainMenu.java
+++ src/main/java/me/nikkcom/owlWarps/GUI/menus/PwarpMainMenu.java
@@ -4,13 +4,22 @@ import me.nikkcom.owlWarps.GUI.Menu;
 import me.nikkcom.owlWarps.utils.ItemCreator;
 import org.bukkit.Material;
 import org.bukkit.event.inventory.ClickType;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Set;
 
 public class PwarpMainMenu extends Menu {
     public PwarpMainMenu() {
         super(54, "Pwarp Main Menu", "PwarpMainMenu");
+        ItemStack PURPLE_GLASS = ItemCreator.create(Material.PURPLE_STAINED_GLASS_PANE, "");
+        Set<Integer> PURPLE_GLASS_SLOTS = Set.of(0, 2, 6, 8, 18, 26, 27, 35, 45, 47, 51, 53);
 
-        setItem(5, ItemCreator.create(Material.REDSTONE, "Redstone breh"), (p, event) -> {
+        ItemStack MAGENTA_GLASS = ItemCreator.create(Material.MAGENTA_STAINED_GLASS_PANE, "");
+        Set<Integer> MAGENTA_GLASS_SLOTS = Set.of(1, 3, 5, 7, 9, 17, 36, 44, 46, 48, 50, 52);
 
-        });
+        for (Integer i : PURPLE_GLASS_SLOTS) setItem(i, PURPLE_GLASS);
+        for (Integer i : MAGENTA_GLASS_SLOTS) setItem(i, MAGENTA_GLASS);
+
+
     }
 }
