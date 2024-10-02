77e01ddf997fe06d9ff89e854d1594f6fc8f53fb
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2024-10-02 19:20:20 +0100

    Update Messages.java

--- src/main/java/me/nikkcom/owlWarps/configuration/Messages.java
+++ src/main/java/me/nikkcom/owlWarps/configuration/Messages.java
@@ -5,18 +5,13 @@ import java.util.Map;
 
 public class Messages {
 
-    private static Map<String, String> messages = new HashMap<>();
+    private static Map<String, String> messages;
+
+    public static void initMessages(Map<String, String> msg) {
+        messages = msg;
+    }
 
     public static String getMessage(String key) {
-        if (messages.containsKey(key)) return messages.get(key);
-        return String.format("Could not find the json key '%s'. Please review the locale files!", key);
-    }
-
-    public static void setMessage(String key, String message) {
-        messages.put(key, message);
-    }
-
-    public static void initMessages(Map<String, String> initialMessages) {
-        messages.putAll(initialMessages);
+        return messages.getOrDefault(key, "Missing Message: " + key); // Provide a default message if not found
     }
 }
